3558455cfb82ba280f618a0e31ebd8eddc00d03f
Paul <chorkunxin@gmail.com>
2023-09-27 14:22:17 +0800

    Added sugar level button

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,22 @@ import Box from './Box';
 function App() {
 
   // the first argument of useState is the default value
+  // but if the state is already given a value previously, then get the current value of the state
+  // two return values
+  // 1. the current value of the state variable OR if it is not initialized, the default value
+  // 2. a function, a mutator that can change the state value
   const [favoriteBeverage, setFavoriteBeverage] = useState("");
+  const [sugarLevel, setSugarLevel] = useState("low");
 
   const selectBubbleTea = () => {
     setFavoriteBeverage("Bubble Tea");
+  }
+
+  let color = "";
+  if (favoriteBeverage === "Coffee") {
+    color = "brown";
+  } else if (favoriteBeverage === "Bubble Tea") {
+    color = "pink"
   }
 
   return (
@@ -19,10 +31,22 @@ function App() {
       <button onClick={selectBubbleTea}>Choose Bubble Tea</button>
       <button onClick={()=>setFavoriteBeverage("Coffee")}>Choose Coffee</button>
       
-      <Box bgcolor="azure" borderColor="black">
+      <div>
+        <h2>Sugar Level</h2>
+        <button onClick={()=>setSugarLevel("low")}>Low</button>
+        <button onClick={()=>setSugarLevel("medium")}>Medium</button>
+        <button onClick={()=>setSugarLevel("high")}>High</button>
+      </div>
+
+      <Box bgcolor="azure" borderColor={color}>
+        <p>Your favorite drink is {favoriteBeverage} and your preferred sugar level is {sugarLevel}</p>
+      </Box>
+      
+      <Box bgcolor="azure" borderColor={ favoriteBeverage === "Coffee" ? "brown" : "pink"}>
         <p>Your favorite drink is {favoriteBeverage}</p>
       </Box>
       
+
       {/* <Box bgcolor="red" borderColor="yellow">She sells seashell</Box>
       <Box bgcolor="purple" borderColor="red">Jack and Jill</Box>
       <Box bgcolor="blue" borderColor="orange">
--- src/Box.jsx
+++ src/Box.jsx
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 /*
 Requirements for a function to be a component
 1. The first alphabet must be upper case
@@ -7,12 +9,16 @@ Props: are data passed into a componnet
   - props are objects used like dictionary
 */
 function Box(props) {
-    return (<div style={{
+
+    const [timesClicked, setTimesClicked] = useState(0);
+
+    return (<div onClick={()=>setTimesClicked(timesClicked+1)}style={{
         border:"6px solid " + props.borderColor,
         backgroundColor: props.bgcolor
     }}>
         {/* props.children all the children elements nested into the component */}
         {props.children}
+        <p>Number of times clicked = {timesClicked}</p>
     </div>);
 }
 
